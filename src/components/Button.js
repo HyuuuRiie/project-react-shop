7403bb0ebd1f7d0ea39019813b80fcbd81cd9ddf
HyuuuRiie <wahyuseptiadifajri11@gmail.com>
2022-05-22 16:15:52 +0700

    edit detail product

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const ButtonContainer = styled.button`
   text-transform: capitalize;
   font-size: 1.4rem;
+  border-radius: 10px;
   background: transparent;
   color: var(--mainWhite);
   &:hover {
